6e4d449a464efeeaf34828bd92b99f74c4931d87
Jui Hsu <artistray@gmail.com>
2021-10-05 23:42:46 -0400

    added images and click function

---
+++ index.js
@@ -0,0 +1,35 @@
+const BASE_URL = ("http://localhost:3000/dishes")
+
+fetch(BASE_URL)
+    .then(resp => resp.json())
+    .then(dish => {
+        dish.forEach(renderFood);
+        console.log(dish);
+    });
+    
+
+//to display each item on the menu
+function renderFood(dish){
+
+    //grab menu div    
+    const dishMenu = document.getElementById("mexican-menu");
+
+    //create image elements
+    const images = document.createElement('img');
+    
+    //link image source
+    images.src = dish.image;
+
+    //append the image
+    dishMenu.appendChild(images);
+
+    //click events
+    images.addEventListener("click", () => {
+        document.getElementById("detail-image").src = dish.image;
+        document.getElementById("name").textContent = dish.name;
+        document.getElementById("description").textContent = dish.description;
+        document.getElementById("price").textContent = "$" + dish.price;
+    })
+}
+
+
